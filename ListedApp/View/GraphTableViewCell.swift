59bb73872a1c05540b55dae0a00209b5013f4fa9
Rahul Bawane <rb.270995@gmail.com>
2023-06-16 00:01:03 +0530

    :tada: adding project to git

---
+++ ListedApp/View/GraphTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GraphTableViewCell.swift
+//  ListedApp
+//
+//  Created by Rahul Bawane on 15/06/23.
+//
+
+import UIKit
+
+class GraphTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ListedApp/View/UserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserTableViewCell.swift
+//  ListedApp
+//
+//  Created by Rahul Bawane on 15/06/23.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
